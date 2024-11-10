a314b72ee6d6304a188336e732b588dae30da8ac
giolive <128484869+giolive@users.noreply.github.com>
2024-11-10 00:08:00 +0000

    Agora com teste

--- idioma/idioma.py
+++ idioma/idioma.py
@@ -1,20 +1,23 @@
 ## Modulo para calculo de porcentagem para jogos de idioma portugues.
 ## Reutilizei ideias do modulo percentage
+
 def idioma_percentages(games):
-  audioP_count = 0
-  languageP_count = 0
+    languageP_count = 0
+    port2020_count = 0
 
-  for game in games:
-      if 'Portuguese - Brazil' in game['supLanguage']:
-          languageP_count += 1
-      if 'Portuguese - Brazil' in game['audio']:
-          audioP_count += 1
+    for game in games:
+        if 'Portuguese - Brazil' in game['language']:
+            languageP_count += 1
+            if game ['release_date']!= '':
+                  year = game ['release_date'].split(',')[1].strip()
+                  if year >= '2020':
+                      port2020_count += 1
 
-  total_games = len(games)
-  if total_games == 0:
-      return 0, 0
+    total_games = len(games)
+    if total_games == 0:
+        return 0, 0
 
-  languageP_percent = (languageP_count / total_games) * 100
-  audioP_percent = (audioP_count / total_games) * 100
-
-return languageP_percent, audioP_percent
+    languageP_percent = (languageP_count / total_games) * 100
+    port2020_percent = (port2020_count / total_games) * 100
+    
+    return languageP_percent, port2020_percent
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import csv
 from calculos.percentage import game_percentages
+from idioma.idioma import idioma_percentages
 from collections import Counter
 
 ## Iniciando importação do arquivo csv com o csv reader
 ## comecei com um arquivo com apenas 30 linhas pois o csv inteiro é muito grande.
-with open('steam_games.csv') as csv_file:
+with open('teste.csv') as csv_file:
   csv_reader = csv.reader(csv_file, delimiter=',')
   next(csv_reader)
   lines = [[element.strip() for element in row] for row in csv_reader]
@@ -18,8 +19,7 @@ games = [
       'name': line[1],
       'release_date': line[2],
       'price': float(line[6]),
-      'supLanguage': line[9],
-      'audio': line[10]
+      'language' : line[9]
   }
   for line in lines
 ]
@@ -29,7 +29,8 @@ games = [
 if len(games) > 0:
   primeiro = games[0]['name']
   preco1 = games[0]['price']
-  print("Primeiro da lista: ", primeiro, "Preço: ", preco1)
+  lang = games[0]['language']
+  print("Primeiro da lista: ", primeiro, "Preço: ", preco1, "Idiomas: ", lang)
 else:
   raise Exception ("No game available")
 
@@ -50,9 +51,9 @@ for game in games:
   if release_date != '':
       year = release_date.split(',')[1].strip()  
       year_count[year] += 1
-else:
-  if not games:
-      raise Exception("No game available")
+  else:
+    if not games:
+        raise Exception("No game available")
 
 if year_count:
   most_common_years = [year for year, count in year_count.most_common() if count == year_count.most_common(1)[0][1]]
@@ -61,3 +62,6 @@ else:
   print("ERRO, nenhum ano encontrado na contagem")
 
 ## Percentual de jogos que possuem Portugues ( Portuguese - Brazil) como idioma no audio ou linguagem suportada.
+languageP_percent, port2020_percent = idioma_percentages(games)
+print(f"Percentual do total de jogos com Portugues como idioma suportado: {languageP_percent:.2f}%")
+print(f"Percentual de jogos com Português, lançados nos últimos 4 anos: {port2020_percent:.2f}%")
